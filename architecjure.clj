b3d5c367b0c6bdffb2318fa588bcba8ec16fd984
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-10-24 00:23:58 -0400

    Added example data structure

--- architecjure.clj
+++ architecjure.clj
@@ -1,13 +1,21 @@
+;;; Data structures
 (defstruct example-group :type :description :examples)
 (defmacro create-example-group [description examples]
   `(struct example-group :example-group ~description ~examples))
 
+(defstruct example :type :description :behavior)
+(defmacro create-example [description behavior]
+  `(struct example :example ~description ~behavior))
+
+;;; Public interface
 (defmacro describe [description & body]
   `(create-example-group ~description (list ~@body)))
 
 (defmacro it [description & behavior]
-  ())
+  `(create-example ~description "mock behavior"))
 
 (defn run-examples [& example-groups]
   (doseq example-group example-groups
     (println example-group)))
+
+;;; Output
