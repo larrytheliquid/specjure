bd630927cb0ba76dd9a9641010054246883e0531
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-10-26 01:12:13 -0400

    Initial workings of a lazy version of architecjure

--- architecjure.clj
+++ architecjure.clj
@@ -1,8 +1,4 @@
 ;;; Data structures
-(defstruct unit :type :example-groups)
-(defmacro create-unit [example-groups]
-  `(struct unit :unit ~example-groups))
-
 (defstruct example-group :type :description :examples)
 (defmacro create-example-group [description examples]
   `(struct example-group :example-group ~description ~examples))
@@ -15,18 +11,19 @@
 (defmacro create-expectation [expected actual]
   `(struct expectation :expectation ~expected ~actual))
 
-;;; Verification
 (defmulti verify :type)
-(defmethod verify :unit [unit]
-  (doseq example-group (:example-groups unit) (verify example-group)))
 
 (defmethod verify :example-group [example-group]
   (printf "%n%s%n"(:description example-group))
-  (doseq example (:examples example-group) (verify example)))
+  (map (fn [example] 
+	 (assoc example :description 
+		(str (:description example-group) " " (:desription example))))
+       (map verify (:examples example-group))))
 
 (defmethod verify :example [example]
-  (printf "- %s%s%n" (:description example)
-	             (if (verify (:behavior example)) "" (format " (FAILED)"))))
+  (let [failed-expectation (verify (:behavior example))]
+    (printf "- %s%s%n" (:description example) (if failed-expectation "" " FAILED"))
+    (assoc example :failed-expectation failed-expectation)))
 
 (defmethod verify :expectation [expectation]
   (= (:expected expectation) (:actual expectation)))
@@ -42,4 +39,6 @@
   `(create-expectation ~expected ~actual))
 
 (defn run-examples [& example-groups]
-  (verify (create-unit example-groups)))
+  (let [examples (map verify example-groups)
+	examples-count (count examples)]
+    (printf "%n%s Examples%n" examples-count)))
