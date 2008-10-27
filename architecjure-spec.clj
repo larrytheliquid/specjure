4c7b7982a783c9bbd43963509157b1d1060bd29a
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-10-27 19:02:05 -0400

    Changed semantics from describe/it/example-group/examples to architect/it/requirements, and put architecture in place to easily add new options to architect

--- architecjure-spec.clj
+++ architecjure-spec.clj
@@ -1,42 +1,48 @@
 (load-file "architecjure.clj")
 
 (defn it-function []
-  (it "should run example when called inside a describe"
+  (it "should run requirement when called inside an architect"
     (=> (* 4 5 6) should = 120)))
 
-(run-examples
+(verify-requirements
 
-(describe "describe, without examples")
+(architect "architect, without requirements")
 
-(describe "describe, with examples"
-  (it "should run a single-line example"
+(architect "architect, with requirements"
+  (it "should run a single-line requirement"
     (=> (concat [1 2] [3 4]) should = [1 2 3 4]))
 
-  (it "should run a multi-line example"
+  (it "should run a multi-line requirement"
     (inc 2)
     (=> (inc 2) should = 3))
 
-  (it "should run a multi-expectation example"
+  (it "should run a multi-expectation requirement"
     (=> 1 should = 1)
     (=> 2 should = 2)))
 
-(describe "describe, with examples in functions"
+(architect "architect, with requirements in functions"
   (it-function))
 
-(describe "describe, with root level"
-  (it "should run examples in the root level"
+(architect "architect, with root level"
+  (it "should run requirements in the root level"
     (=> 1 should = 1))
   
-  (describe "with nesting"
-    (it "should run examples in a nested level"
+  (architect "with nesting"
+    (it "should run requirements in a nested level"
       (=> 1 should = 1))
 
-    (describe "with deeper nesting"
-      (it "should run examples in the deepest level"
+    (architect "with deeper nesting"
+      (it "should run requirements in the deepest level"
 	(=> 1 should = 1))))
 
-  (describe "with separate nesting"
-    (it "should run separately nested examples"
+  (architect "with separate nesting"
+    (it "should run separately nested requirements"
       (=> 1 should = 1))))
+
+(architect "architect-requirement, with description"
+  (it "should return the requirement prepended by the description"
+    (let [requirement (struct requirement "description")
+	  architected-requirement (architect-requirement {:description "modified"} requirement)]
+      (=> (:description architected-requirement) should = "modified description"))))
 
 )
--- architecjure.clj
+++ architecjure.clj
@@ -1,38 +1,49 @@
 ;;; Data structures
-(def *example-groups*)
 (def *failed-expectations*)
-(defstruct example :description :behavior)
+(defstruct requirement :description :behavior)
 (defstruct expectation :expected :actual)
 
+;;; Data structure modifiers
+(def architect-fn-map
+     {:description (fn [new old] (str new " " old))})
+
+(defn architect-requirement [options requirement]
+  (apply assoc requirement (mapcat (fn [option] [option ((option architect-fn-map) 
+						     (option options) 
+						     (option requirement))])
+			       (keys options))))
+
 ;;; Public interface
-(defmacro describe [description & body]
-  `(map (fn [example#]
-	  (assoc example# :description (str ~description " " (:description example#))))
-	(flatten (list ~@body))))
+(defmacro architect [options & body]
+  `(let [options# (if (string? ~options) {:description ~options} ~options)]
+     (map (fn [requirement#] (architect-requirement options# requirement#)) 
+	  (flatten (list ~@body)))))
 
 (defmacro it [description & behavior]
-  `(struct example ~description (fn [] (binding [*failed-expectations* []]
+  `(struct requirement ~description (fn [] (binding [*failed-expectations* []]
 					 ~@behavior
 					 *failed-expectations*))))
 
 (defmacro => [expected should matcher actual]
   `(let [expectation# (struct expectation ~expected ~actual)]
-     (when-not (passed? expectation#)
+     (when-not (satisfied? expectation#)
        (push! *failed-expectations* expectation#))))
 
-(defmacro run-examples [& body]
-  `(let [examples# (map run (concat ~@body))
-	 examples-count# (count examples#)
-	 failures-count# (count (filter :failed-expectations examples#))]
-     (printf "%n%s Examples, %s Failures%n" examples-count# failures-count#)))
+(defmacro verify-requirements [& body]
+  `(let [requirements# (map verify (concat ~@body))
+	 requirements-count# (count requirements#)
+	 failures-count# (count (filter :failed-expectations requirements#))]
+     (printf "%n%s Requirements, %s Failures%n" requirements-count# failures-count#)))
 
 ;;; Verification
-(defn run [example]
-  (let [failed-expectations ((:behavior example)) example-passed? (empty? failed-expectations)]
-    (printf "%s%s%n" (:description example) (if example-passed? "" " (FAILED)"))
-    (if example-passed? example (assoc example :failed-expectations failed-expectations))))
+(defn verify [requirement]
+  (let [failed-expectations ((:behavior requirement)) 
+	requirement-satisfied? (empty? failed-expectations)]
+    (printf "%s%s%n" (:description requirement) (if requirement-satisfied? "" " (FAILED)"))
+    (if requirement-satisfied? requirement 
+	(assoc requirement :failed-expectations failed-expectations))))
 
-(defn passed? [expectation]
+(defn satisfied? [expectation]
   (= (:expected expectation) (:actual expectation)))
 
 ;;; Utilities
