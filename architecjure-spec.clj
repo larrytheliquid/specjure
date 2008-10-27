6ee6961185aaa3dbb8fa88913b15dc087c1d1bcd
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-10-26 23:20:38 -0400

    Converted to more of a functional style approach

--- architecjure-spec.clj
+++ architecjure-spec.clj
@@ -1,19 +1,13 @@
 (load-file "architecjure.clj")
 
+;; (defn it-function []
+;;   (it "should run example when called inside a describe"
+;;     (=> (* 4 5 6) should = 120)))
+
+;; (describe "describe, with examples in functions"
+;;   (it-function))
+
 (run-examples
-
-(def my-vector)
-(describe "push!, with an empty vector"
-  (it "should add the value to the list"
-    (binding [my-vector []]
-      (push! my-vector 1)
-      (=> my-vector should = [1]))))
-
-(describe "push!, with a non-empty vector"
-  (it "should add the value to the back of the list"
-    (binding [my-vector [1]]
-      (push! my-vector 2)
-      (=> my-vector should = [1 2]))))
 
 (describe "describe, without examples")
 
@@ -28,12 +22,5 @@
   (it "should run a multi-expectation example"
     (=> 1 should = 1)
     (=> 2 should = 2)))
-
-(defn it-function []
-  (it "should run example when called inside a describe"
-    (=> (* 4 5 6) should = 120)))
-
-(describe "describe, with examples in functions"
-  (it-function))
 
 )
--- architecjure.clj
+++ architecjure.clj
@@ -1,57 +1,40 @@
-;;; Utilities
-(defmacro push! [coll x]
-  (list 'set! coll (list 'conj coll x)))
-
 ;;; Data structures
 (def *example-groups*)
 (def *failed-expectations*)
-
-(defstruct example-group :type :description :examples)
-(defmacro create-example-group [description examples]
-  `(struct example-group :example-group ~description ~examples))
-
-(defstruct example :type :description :behavior)
-(defmacro create-example [description behavior]
-  `(struct example :example ~description ~behavior))
-
-(defstruct expectation :type :expected :actual :result)
-(defmacro create-expectation [expected actual]
-  `(struct expectation :expectation ~expected ~actual))
-
-;;; Verification
-(defmulti verify :type)
-(defmethod verify :example-group [example-group]
-  (map verify (map (fn [example] 
-		     (assoc example :description 
-			    (str (:description example-group) " " (:description example))))
-		   (:examples example-group))))
-
-(defmethod verify :example [example]
-  (let [failed-expectations ((:behavior example)) example-passed? (empty? failed-expectations)]
-    (printf "%s%s%n" (:description example) (if example-passed? "" " (FAILED)"))
-    (if example-passed? example (assoc example :failed-expectations failed-expectations))))
-
-(defmethod verify :expectation [expectation]
-  (= (:expected expectation) (:actual expectation)))
+(defstruct example :description :behavior)
+(defstruct expectation :expected :actual)
 
 ;;; Public interface
 (defmacro describe [description & body]
-  `(push! *example-groups* (create-example-group ~description (list ~@body))))
+  `(map (fn [example#]
+	  (assoc example# :description (str ~description " " (:description example#))))
+	(list ~@body)))
 
 (defmacro it [description & behavior]
-  `(create-example ~description (fn [] (binding [*failed-expectations* []]
+  `(struct example ~description (fn [] (binding [*failed-expectations* []]
 					 ~@behavior
 					 *failed-expectations*))))
 
 (defmacro => [expected should matcher actual]
-  `(let [expectation# (create-expectation ~expected ~actual)]
-     (when-not (verify expectation#)
+  `(let [expectation# (struct expectation ~expected ~actual)]
+     (when-not (passed? expectation#)
        (push! *failed-expectations* expectation#))))
 
 (defmacro run-examples [& body]
-  `(binding [*example-groups* []]
-     (do ~@body)
-     (let [examples# (mapcat verify *example-groups*)
-	   examples-count# (count examples#)
-	   failures-count# (count (filter :failed-expectations examples#))]
-       (printf "%n%s Examples, %s Failures%n" examples-count# failures-count#))))
+  `(let [examples# (map run (concat ~@body))
+	 examples-count# (count examples#)
+	 failures-count# (count (filter :failed-expectations examples#))]
+     (printf "%n%s Examples, %s Failures%n" examples-count# failures-count#)))
+
+;;; Verification
+(defn run [example]
+  (let [failed-expectations ((:behavior example)) example-passed? (empty? failed-expectations)]
+    (printf "%s%s%n" (:description example) (if example-passed? "" " (FAILED)"))
+    (if example-passed? example (assoc example :failed-expectations failed-expectations))))
+
+(defn passed? [expectation]
+  (= (:expected expectation) (:actual expectation)))
+
+;;; Utilities
+(defmacro push! [coll x]
+  (list 'set! coll (list 'conj coll x)))
