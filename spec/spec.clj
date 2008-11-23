c588f233033203bed0ce86ebab8715fded7941fd
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-11-23 01:31:00 -0500

    Put specjure main source in a "src" directory

--- spec/spec.clj
+++ spec/spec.clj
@@ -16,11 +16,11 @@
 ;;; TODO: Add file and line number to failing spec information
 ;;; TODO: A sync-before, wrapping the before in dosync so you can use its body
 
-(load-file "specjure/utilities/utilities.clj")
-(load-file "specjure/expectations/expectations.clj")
-(load-file "specjure/examples/examples.clj")
-(load-file "specjure/options/options.clj")
-(load-file "specjure/public/public.clj")
+(load-file "src/specjure/utilities/utilities.clj")
+(load-file "src/specjure/expectations/expectations.clj")
+(load-file "src/specjure/examples/examples.clj")
+(load-file "src/specjure/options/options.clj")
+(load-file "src/specjure/public/public.clj")
 
 (load-file "spec/describe/default_spec.clj")
 (load-file "spec/describe/before_spec.clj")
