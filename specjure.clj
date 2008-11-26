487038ab68c86ad1ef745a541c52a491dfbde13a
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-11-26 06:28:54 -0500

    Extracted out printing failure summary

--- specjure.clj
+++ specjure.clj
@@ -28,12 +28,17 @@
 	(= matcher 'be-true) [true (not (not (first arguments)))]
 	(= matcher 'be-false) [false (not (not (first arguments)))]))
 
+(defn format-failure [example expectation]
+  (format "%n'%s' FAILED%nexpected: %s%ngot: %s (using =)%n" 
+	  (:description example)
+	  (:expected expectation)
+	  (:actual expectation)))
+
 (defn check [example]
   (let [failed-expectations ((:fn example))
 	example-passed? (empty? failed-expectations)]
     (print (if example-passed? "." "F"))
-    (if example-passed? example 
-	(assoc example :failed-expectations failed-expectations))))
+    (if example-passed? true (format-failure example (first failed-expectations)))))
 
 ;;; Options
 (defmulti option :option-name)
@@ -64,10 +69,10 @@
 
 (defmacro it [description & body]
   `(dosync (commute *examples* conj
-		    (struct example (str *description* " " ~description)
-			    #(binding [*failed-expectations* []]
-			       ~@body
-			       *failed-expectations*)))))
+	   (struct example (str *description* " " ~description)
+		   #(binding [*failed-expectations* []]
+		      ~@body
+		      *failed-expectations*)))))
 
 (defn- _should [comparator matcher arguments]
   `(let [expectation# (apply struct expectation ~comparator (parse-matcher '~matcher ~@arguments))]
@@ -83,14 +88,7 @@
 (defn check-examples 
   ([] (check-examples @*examples*))
   ([body]                    
-     (dosync (ref-set *examples* []))
-     (let [examples (map check body)
-	   examples-count (count examples)
-	   failures-count (count (filter :failed-expectations examples))]
-       (printf "%n%n%s Examples, %s Failures%n" examples-count failures-count)
-       (doseq failed-example (filter :failed-expectations examples)
-	 (doseq failed-expectation (:failed-expectations failed-example)
-	   (printf "%n'%s' FAILED%nexpected: %s%ngot: %s (using =)%n" 
-		   (:description failed-example)
-		   (:expected failed-expectation)
-		   (:actual failed-expectation)))))))
+     (let [failures (filter string? (map check body))]    
+       (dosync (ref-set *examples* []))
+       (printf "%n%n%s Examples, %s Failures%n" (count body) (count failures))
+       (doseq failure failures (print failure)))))
