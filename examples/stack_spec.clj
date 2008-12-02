53fe6c783ee6790df969b2b1242956ca5ddc4fbf
Larry Diehl <larrytheliquid@larry-diehls-macbook.local>
2008-12-02 00:49:07 -0500

    Changed example namespace to specjure.examples

--- examples/stack_spec.clj
+++ examples/stack_spec.clj
@@ -1,7 +1,7 @@
 ;;; RSpec stack example: http://rspec.info/examples.html
 
 ;;; Implementation
-(ns example 
+(ns specjure.examples 
   (:refer-clojure :exclude [empty? peek]) 
   (:use specjure))
 
@@ -29,30 +29,30 @@
       result))
 
 ;;; Specs
-(shared-examples-for "non-empty example/stack" []
+(shared-examples-for "non-empty specjure.examples/stack" []
   (it "is not empty"
     (should not be empty (param :stack)))
 
-  (it "returns the top item when applied to example/peek"
+  (it "returns the top item when applied to specjure.examples/peek"
     (should = (param :last-item-added) (peek (param :stack))))
 
-  (it "does not remove the top item when applied to example/peek"
+  (it "does not remove the top item when applied to specjure.examples/peek"
     (should = (param :last-item-added) (peek (param :stack)))
     (should = (param :last-item-added) (peek (param :stack))))
 
-  (it "returns the top item when applied to example/pop!"
+  (it "returns the top item when applied to specjure.examples/pop!"
     (should = (param :last-item-added) (pop! (param :stack))))
   
-  (it "removes the top item when applied to example/pop!"
+  (it "removes the top item when applied to specjure.examples/pop!"
     (should = (param :last-item-added) (pop! (param :stack)))
     (when-not (empty? (param :stack))
       (should not = (param :last-item-added) (pop! (param :stack))))))
 
-(shared-examples-for "non-full example/stack" []
+(shared-examples-for "non-full specjure.examples/stack" []
   (it "is not full"
     (should not be full (param :stack)))
 
-  (it "adds to the top when applied to example/push!"
+  (it "adds to the top when applied to specjure.examples/push!"
     (push! (param :stack) "newly added top item")
     (when-not (empty? (param :stack))
       (should = "newly added top item" (peek (param :stack))))))
@@ -64,13 +64,13 @@
     (it "is empty"
       (should be empty (param :stack)))
 
-    (it-should-behave-like "non-full example/stack")
+    (it-should-behave-like "non-full specjure.examples/stack")
 
-    (it "complains when applied to example/peek"
+    (it "complains when applied to specjure.examples/peek"
       (should throw java.util.EmptyStackException 
 	(peek (param :stack))))
 
-    (it "complains when applied to example/pop!"
+    (it "complains when applied to specjure.examples/pop!"
       (should throw java.util.EmptyStackException 
 	(pop! (param :stack)))))
 
@@ -79,16 +79,16 @@
       (push! (param :stack) 3)
       (set-param :last-item-added 3))
 
-    (it-should-behave-like "non-empty example/stack")
-    (it-should-behave-like "non-full example/stack"))
+    (it-should-behave-like "non-empty specjure.examples/stack")
+    (it-should-behave-like "non-full specjure.examples/stack"))
 
   (describe "(with one item less than capactiy)"
     (before-each
       (doseq i (range 1 10) (push! (param :stack) i))
       (set-param :last-item-added 9))
 
-    (it-should-behave-like "non-empty example/stack")
-    (it-should-behave-like "non-full example/stack"))
+    (it-should-behave-like "non-empty specjure.examples/stack")
+    (it-should-behave-like "non-full specjure.examples/stack"))
 
   (describe "(full)"
     (before-each
@@ -98,8 +98,8 @@
     (it "is full"
       (should be full (param :stack)))
 
-    (it-should-behave-like "non-empty example/stack")
+    (it-should-behave-like "non-empty specjure.examples/stack")
 
-    (it "complains when applied to example/push!"
+    (it "complains when applied to specjure.examples/push!"
       (should throw Exception 
 	(push! (param :stack) (fn []))))))
